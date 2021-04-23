cf3a7a6eee441ed252bc7886c263171b55afc7fd
Hila Manor <hila8manor@gmail.com>
2021-04-23 15:40:18 +0300

    started adding loss functions!
    completed more of the main single-scale training function

---
+++ code/loss_model.py
@@ -0,0 +1,101 @@
+import torch
+import torch.nn as nn
+import style_loss
+import contextual_loss
+import pd_loss
+
+vgg_normalization_mean = torch.tensor([0.485, 0.456, 0.406])
+vgg_normalization_std = torch.tensor([0.229, 0.224, 0.225])
+
+STYLE_LAYERS_TRANSLATION = {'conv_1': 'conv1_1',
+                            'conv_2': 'conv1_2',
+
+                            'conv_3': 'conv2_1',
+                            'conv_4': 'conv2_2',
+
+                            'conv_5': 'conv3_1',
+                            'conv_6': 'conv3_2',
+                            'conv_7': 'conv3_3',
+                            'conv_8': 'conv3_4',
+
+                            'conv_9': 'conv4_1',
+                            'conv_10': 'conv4_2',
+                            'conv_11': 'conv4_3',
+                            'conv_12': 'conv4_4',
+
+                            'conv_13': 'conv5_1',
+                            'conv_14': 'conv5_2',
+                            'conv_15': 'conv5_3',
+                            'conv_16': 'conv5_4'}
+
+
+class Normalization(nn.Module):
+    """create a module to normalize input image so we can easily put it in a nn.Sequential"""
+    def __init__(self, mean, std):
+        super(Normalization, self).__init__()
+        # .view the mean and std to make them [C x 1 x 1] so that they can
+        # directly work with image Tensor of shape [B x C x H x W].
+        # B is batch size. C is number of channels. H is height and W is width.
+        self.mean = torch.tensor(mean).view(-1, 1, 1)
+        self.std = torch.tensor(std).view(-1, 1, 1)
+
+    def forward(self, img):
+        """normalize img"""
+        return (img - self.mean) / self.std
+
+def generate_loss_block(vgg, real_img, mode, chosen_layers, opt):
+    # TODO: vgg = copy.deepcopy(vgg)
+
+    # normalization module
+    normalization = Normalization(opt.normalization_mean.to(opt.device),
+                                  opt.normalization_std.to(opt.device)).to(opt.device)
+
+    # an iterable access to or list of content/syle losses
+    layers_losses = []
+
+    # assuming that vgg is a nn.Sequential, so we make a new nn.Sequential
+    # to put in modules that are supposed to be activated sequentially
+    model = nn.Sequential(normalization)
+
+    i = 0  # increment every time we see a conv
+    for layer in vgg.children():
+        if isinstance(layer, nn.Conv2d):
+            i += 1
+            name = 'conv_{}'.format(i)
+        elif isinstance(layer, nn.ReLU):
+            name = 'relu_{}'.format(i)
+            # The in-place version doesn't play very nicely with the ContentLoss
+            # and StyleLoss we insert below. So we replace with out-of-place ones here.
+            layer = nn.ReLU(inplace=False)
+        elif isinstance(layer, nn.MaxPool2d):
+            name = 'pool_{}'.format(i)
+        elif isinstance(layer, nn.BatchNorm2d):
+            name = 'bn_{}'.format(i)
+        else:
+            raise RuntimeError('Unrecognized layer: {}'.format(layer.__class__.__name__))
+
+        model.add_module(name, layer)
+
+        # Attach losses to the chosen layers
+        if name.startswith('conv') and STYLE_LAYERS_TRANSLATION[name] in chosen_layers:
+            target_feature = model(real_img).detach()
+            if mode.lower() == 'style':
+                loss_f = style_loss.StyleLoss
+            elif mode.lower() == 'contextual':
+                loss_f = contextual_loss.ContextualLoss
+            elif mode.lower() == 'pdl':
+                loss_f = pd_loss.PDLoss
+            else:
+                raise RuntimeError(f'Unrecognized loss: {mode}')
+
+            loss = loss_f(target_feature, device=opt.device)
+            model.add_module(f"{mode.lower()}_loss_{i}", loss)
+            layers_losses.append(loss)
+
+    # now we trim off the layers after the last losses
+    for i in range(len(model) - 1, -1, -1):
+        if isinstance(model[i], loss_f):
+            break
+
+    model = model[:(i + 1)]
+    return model, layers_losses
--- code/training.py
+++ code/training.py
@@ -1,5 +1,6 @@
 import models
 import functions
+import torch
 import torch.nn as nn
 import torch.optim as optim
 
@@ -11,10 +12,9 @@ def train(opt, Gs, Zs, reals, NoiseAmp):
     nfc = 32 # / 64? TODO
     min_nfc = 32 # / 64? TODO
 
-    G = init_generator(opt)
+    curr_G = init_generator(opt)
 
-
-    G, z_curr = train_single_scale(G, opt)
+    G, z_curr = train_single_scale(Gs, curr_G, real_imgs, opt)
 
 
 
@@ -27,7 +27,7 @@ def init_generator(opt):
 
 
 
-def train_single_scale(Generators, real_imgs, opt):
+def train_single_scale(Generators, curr_G, real_imgs, opt):
     real_img = real_imgs[len(Generators)]
     opt.nzx = real_img.shape[2]  # Width of image in current scale
     opt.nzy = real_img.shape[3]  # Height of image in current scale
@@ -42,75 +42,85 @@ def train_single_scale(Generators, real_imgs, opt):
     # TODO-FUTURE create noise (and pad it?)
 
     # Setup Optimizer
-    optimizer = optim.Adam(G.parameters(), lr=0.0005, betas=(0.5, 0.999))
+    optimizer = optim.Adam(curr_G.parameters(), lr=0.0005, betas=(0.5, 0.999))
     scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[1000],
                                                gamma=0.1)
 
     # TODO arrays for errors for graphs
     style_loss = []
 
+    # TODO-FUTURE currently only for generic implementation (1 scale)
+    prev = torch.full([1, opt.nc_z, opt.nzx, opt.nzy], 0, device=opt.device)
+    # TODO in_s = prev
+    prev = image_pad_func(prev)
+    z_prev = torch.full([1, opt.nc_z, opt.nzx, opt.nzy], 0, device=opt.device)
+    z_prev = noise_pad_func(z_prev)
+    opt.noise_amp = 1
+
+    # TODO-FUTRE:
+    #   first epoch & first step (D)
+    #       first scale:
+    #           in_s = zeros
+    #           prev = padded zeros (by image)
+    #           z_prev = padded zeros (by noise)
+    #           noise_amp = 1
+    #       else:
+    #           prev = padded draw_concat 'rand' (by image)
+    #               --> prev = previous generated image FROM RANDOM NOISE
+    #           z_prev = padded draw_concat 'rec' (by image)
+    #               --> z_prev = previous generate image FROM KNOWN NOISE
+    #   else (not first epoch || not first step)
+
     for epoch in range(opt.epochs):
         # z_opt is {Z*, 0, 0, 0, ...}. The specific set of input noise maps
         # which generates the original image xn
         z_opt = functions.generate_noise([1, opt.nzx, opt.nzy])
-        z_opt = noise_pad_func(z_opt.expand(1, 3, opt.nzx, opt.nzy))
-
-        # TODO generate noise z_opt
-        # TODO-FUTURE z_opt should only be generated in 1st scale
+        z_opt = noise_pad_func(z_opt.expand(1, opt.nc_z, opt.nzx, opt.nzy))
 
         # noise_ is the input noise (before adding the image or changing the variance)
-        # TODO generate noise noise_
-        # TODO-FUTURE pad noises
+        noise_ = functions.generate_noise([1, opt.nzx, opt.nzy], device=opt.device)
+        noise_ = noise_pad_func(noise_.expand(1, opt.nc_z, opt.nzx, opt.nzy))
+        # Notice that the noise for the 3 RGB channels is the same
+        # TODO-FUTURE z_opt should only be generated in 1st scale
 
-        # TODO for generic implementation, make
-        #       in_s = zeros
-        #       prev = padded zeros (by image)
-        #       z_prev = padded zeros (by noise)
-        #       noise_amp = 1
-        # TODO-FUTRE:
-        #   first epoch & first step (D)
-        #       first scale:
-        #           in_s = zeros
-        #           prev = padded zeros (by image)
-        #           z_prev = padded zeros (by noise)
-        #           noise_amp = 1
-        #       else:
-        #           prev = padded draw_concat 'rand' (by image)
-        #               --> prev = previous generated image FROM RANDOM NOISE
-        #           z_prev = padded draw_concat 'rec' (by image)
-        #               --> z_prev = previous generate image FROM KNOWN NOISE
-        #   else (not first epoch || not first step)
-        #       prev = padded draw_concat 'rand' (by image)
-        #           --> prev = previous generated image FROM (new) RANDOM NOISE
-
-        # TODO noise = noise_
+        noise = noise_
         # TODO-FUTURE if not first scale noise = noise_amp * noise_ + prev
 
-        #  ~~~~~~~~~~~~~~~~~~~~~~~~
+
         # TODO-THINK for every step in G steps
-        #  ~~~~~~~~~~~~~~~~~~~~~~~~
+        for j in range(opt.Gsteps):
+            curr_G.zero_grad()
+            fake_im = curr_G(noise.detach(), prev)  # TODO think on detach
 
-        # TODO zero grad
-        # TODO Forward on Generator network
-        # TODO Add style_loss: fake + real -> LOSS
-        # TODO backward on style_loss
+            # TODO Add style_loss: fake + real -> LOSS
 
-        # TODO-FUTUE implement reconstruction-loss using alpha (We want to ensure that there exists a specific set of input
-        #  noise maps, which generates the original image x)
-        #      if alpha != 0
-        #           Z_opt = noise_amp * z_opt + z_prev
-        #               --> in first scale: z_prev=0, z_opt random every iter
-        #               --> else: z_prev = previous generate image FROM KNOWN NOISE
-        #               -->         z_opt = 0 ({Z*,0,0,0,0,0})
-        #           GDS (train) by reconstruction loss
-        #               --> .
-        #       else:
-        #           Z_opt = z_opt (remember, only in first scale the noise changes every ITER)
-        #               --> = 0 in all but first scale
-        #           rec_loss = 0
-        #
+            loss.backward()  # TODO retain_graph=True
 
+            # TODO-FUTUE implement reconstruction-loss using alpha (We want to ensure that there exists a specific set of input
+            #  noise maps, which generates the original image x)
+            #      if alpha != 0
+            #           Z_opt = noise_amp * z_opt + z_prev
+            #               --> in first scale: z_prev=0, z_opt random every iter
+            #               --> else: z_prev = previous generate image FROM KNOWN NOISE
+            #               -->         z_opt = 0 ({Z*,0,0,0,0,0})
+            #           GDS (train) by reconstruction loss
+            #               --> .
+            #       else:
+            #           Z_opt = z_opt (remember, only in first scale the noise changes every ITER)
+            #               --> = 0 in all but first scale
+            #           rec_loss = 0
+
+            optimizer.step()
         scheduler.step()
 
+        # update prev
+        prev = draw_concat(Generators, 'rand', noise_pad_func, image_pad_func, opt)
+        prev = image_pad_func(prev)
+
     # TODO save network?
-    return G, z_opt
+    return curr_G, z_opt
+
+
+def draw_concat(Generators, mode, noise_pad_func, image_pad_func, opt):
+    # TODO-FUTURE good luck
+    return torch.full([1, opt.nc_z, opt.nzx, opt.nzy], 0, device=opt.device)
