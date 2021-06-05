11d021d6b6733b8981091cd9f665cf01378fa512
Hila Manor <hila8manor@gmail.com>
2021-06-05 19:13:25 +0300

    stupid warning

--- code/loss_model.py
+++ code/loss_model.py
@@ -36,8 +36,8 @@ class Normalization(nn.Module):
         # .view the mean and std to make them [C x 1 x 1] so that they can
         # directly work with image Tensor of shape [B x C x H x W].
         # B is batch size. C is number of channels. H is height and W is width.
-        self.mean = torch.tensor(mean).view(-1, 1, 1)
-        self.std = torch.tensor(std).view(-1, 1, 1)
+        self.mean = mean.clone().detach().view(-1, 1, 1)
+        self.std = std.clone().detach().view(-1, 1, 1)
 
     def forward(self, img):
         """normalize img"""
