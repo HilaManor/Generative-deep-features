17f4052a6018bcd20a36038c4f02584241587581
Hila Manor <hila8manor@gmail.com>
2021-08-14 18:26:54 +0300

    docs + noise in upper scales isn't constant across RGB

--- code/training.py
+++ code/training.py
@@ -55,28 +55,28 @@ def train(out_dir, real_img, scale_factor, total_scales, opt):
         curr_nfc = min(opt.nfc * pow(2, math.floor(scale / 4)), 128)
         curr_min_nfc = min(opt.min_nfc * pow(2, math.floor(scale / 4)), 128)
 
-        curr_G = _init_generator(curr_nfc, curr_min_nfc, opt)
+        curr_g = _init_generator(curr_nfc, curr_min_nfc, opt)
 
         # Learn initial weights guess from previous scale
         if nfc_prev == curr_nfc and opt.try_initial_guess:
             print("Initial weights guess is previous scale")
             prev_out_dir = output_handler.gen_scale_dir(out_dir, scale - 1)
-            curr_G.load_state_dict(torch.load(os.path.join(prev_out_dir, G_WEIGHTS_FILE_NAME)))
+            curr_g.load_state_dict(torch.load(os.path.join(prev_out_dir, G_WEIGHTS_FILE_NAME)))
 
         # Train a single scale
         start_time = time.time()
-        curr_G, z_curr, curr_noise_amp = _train_single_scale(trained_generators, z_opts, noise_amps,
-                                                             curr_G, real_imgs, vgg, scale_out_dir,
-                                                             scale_factor, opt)
+        curr_g, z_curr, curr_noise_amp = _train_single_scale(trained_generators, z_opts,
+                                                             noise_amps, curr_g, real_imgs, vgg,
+                                                             scale_out_dir, scale_factor, opt)
         print(f"{scale} Scale Training Time: {time.time()-start_time}")
 
         # Save the trained generator for future loading
-        torch.save(curr_G.state_dict(), os.path.join(scale_out_dir, G_WEIGHTS_FILE_NAME))
+        torch.save(curr_g.state_dict(), os.path.join(scale_out_dir, G_WEIGHTS_FILE_NAME))
 
         # Append results
-        [p.requires_grad_(False) for p in curr_G.parameters()]
-        curr_G.eval()
-        trained_generators.append(curr_G)
+        [p.requires_grad_(False) for p in curr_g.parameters()]
+        curr_g.eval()
+        trained_generators.append(curr_g)
         z_opts.append(z_curr)
         noise_amps.append(curr_noise_amp)
 
@@ -96,8 +96,8 @@ def _init_generator(curr_nfc, curr_min_nfc, opt):
     """
 
     net_g = models.GeneratorConcatSkip2CleanAdd(curr_nfc, opt.nc, opt.ker_size, opt.padd_size,
-                                               opt.stride, opt.num_layer,
-                                               curr_min_nfc).to(opt.device)
+                                                opt.stride, opt.num_layer,
+                                                curr_min_nfc).to(opt.device)
     net_g.apply(models.weights_init)
     # TODO load from file?
     return net_g
@@ -161,55 +161,58 @@ def _train_single_scale(trained_generators, z_opts, noise_amps, curr_g, real_img
     # z_opt is a padding of {Z*, 0, 0, 0, ...}. The specific set of input noise maps which
     # generates the original image xn
     if cur_scale:
-        prev = _draw_concat(trained_generators, z_opts, real_imgs, noise_amps, 'rand', pad_func,
-                            scale_factor, opt)
-        prev = pad_func(prev)
-        z_prev = _draw_concat(trained_generators, z_opts, real_imgs, noise_amps, 'rec', pad_func,
-                              scale_factor, opt)
+        prev_rand = _draw_concat(trained_generators, z_opts, real_imgs, noise_amps, 'rand',
+                                 pad_func, scale_factor, opt)
+        prev_recon = _draw_concat(trained_generators, z_opts, real_imgs, noise_amps, 'rec',
+                                  pad_func, scale_factor, opt)
         criterion = nn.MSELoss()
-        RMSE = torch.sqrt(criterion(real_img, z_prev))
-        noise_amp = opt.noise_amp * RMSE
-        z_prev = pad_func(z_prev)
+        rmse = torch.sqrt(criterion(real_img, prev_recon))
+        noise_amp = opt.noise_amp * rmse
         if opt.z_opt_zero:
-            z_opt = pad_func(torch.full([1, opt.nc, opt.nzx, opt.nzy], 0, dtype=torch.float32, device=opt.device))
+            z_opt = pad_func(torch.full([1, opt.nc, opt.nzx, opt.nzy], 0, dtype=torch.float32,
+                                        device=opt.device))
         else:
             z_opt = image_processing.generate_noise([1, opt.nzx, opt.nzy], device=opt.device)
             z_opt = pad_func(z_opt.expand(1, opt.nc, opt.nzx, opt.nzy))
+            # Notice that the noise for the 3 RGB channels is the same
     else:
-        prev = torch.full([1, opt.nc, opt.nzx, opt.nzy], 0, device=opt.device)
-        # TODO in_s = prev
-        prev = pad_func(prev)
-        z_prev = torch.full([1, opt.nc, opt.nzx, opt.nzy], 0, device=opt.device)
-        z_prev = pad_func(z_prev)
+        prev_rand = torch.full([1, opt.nc, opt.nzx, opt.nzy], 0, device=opt.device)
+        prev_recon = torch.full([1, opt.nc, opt.nzx, opt.nzy], 0, device=opt.device)
         noise_amp = 1
         z_opt = image_processing.generate_noise([1, opt.nzx, opt.nzy], device=opt.device)
         z_opt = pad_func(z_opt.expand(1, opt.nc, opt.nzx, opt.nzy))
         # Notice that the noise for the 3 RGB channels is the same
+    prev_rand = pad_func(prev_rand)
+    prev_recon = pad_func(prev_recon)
 
-    if cur_scale == 0:
+    # new noise to generate examples from over the epochs
+    if not cur_scale:
+        # In the first scale it should be constant across the channels
         example_noise = image_processing.generate_noise([1, opt.nzx, opt.nzy]).detach()
         example_noise = example_noise.expand(1, opt.nc, opt.nzx, opt.nzy)
     else:
         example_noise = image_processing.generate_noise([opt.nc, opt.nzx, opt.nzy]).detach()
     example_noise = pad_func(example_noise)
 
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ MAIN TRAINING LOOP ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     start_time = time.time()
-    style_rec_factor = 1
     for epoch in range(opt.epochs):
         # noise_ is the input noise (before adding the image or changing the variance)
-        noise_ = image_processing.generate_noise([1, opt.nzx, opt.nzy], device=opt.device)
-        noise_ = pad_func(noise_.expand(1, opt.nc, opt.nzx, opt.nzy))
-        # Notice that the noise for the 3 RGB channels is the same
+        if cur_scale > 0:
+            noise_ = image_processing.generate_noise([opt.nc, opt.nzx, opt.nzy], device=opt.device)
+        else:
+            noise_ = image_processing.generate_noise([1, opt.nzx, opt.nzy], device=opt.device)
+            noise_ = noise_.expand(1, opt.nc, opt.nzx, opt.nzy)
+            # Notice that the noise for the 3 RGB channels is the same
+        noise_ = pad_func(noise_)
 
-        noise = noise_*noise_amp + prev
+        noise = noise_*noise_amp + prev_rand
 
-        # TODO-THINK for every step in G steps
-        loss = 0
         total_loss = 0
         rec_loss = 0
         for j in range(opt.Gsteps):
             curr_g.zero_grad()
-            fake_im = curr_g(noise.detach(), prev)  # TODO think on detach
+            fake_im = curr_g(noise.detach(), prev_rand)  # Generate a fake image
 
             if opt.upsample_for_vgg:
                 fake_im = loss_model.validate_vgg_im_size(fake_im)
@@ -218,12 +221,16 @@ def _train_single_scale(trained_generators, z_opts, noise_amps, curr_g, real_img
                 n_layers = len(loss_model.validate_vgg_layers_amount(
                     fake_im.shape[2:], opt.chosen_layers, opt.min_features))
 
+            # Calculate loss
             loss_block(fake_im)
             if opt.c_alpha != 0:
                 fake_im_patches = loss_model.split_img_to_patches(fake_im, opt.c_patch_size)
-                fake_im_patches_flattened = fake_im_patches.reshape(1, -1, opt.nc * opt.c_patch_size * opt.c_patch_size, 1)
+                fake_im_patches_flattened = fake_im_patches.reshape(1, -1, opt.nc *
+                                                                    opt.c_patch_size *
+                                                                    opt.c_patch_size, 1)
                 c_loss_block(fake_im_patches_flattened)
                 color_loss = c_loss_block.loss
+                color_loss_arr.append(color_loss.detach() if opt.c_alpha else color_loss)
             else:
                 color_loss = 0
 
@@ -231,16 +238,14 @@ def _train_single_scale(trained_generators, z_opts, noise_amps, curr_g, real_img
             for i, sl in enumerate(layers_losses):
                 loss += opt.layers_weights[i] * sl.loss / (n_layers + 1)
             distribution_loss_arr.append(loss.detach())
-            #loss.backward(retain_graph=True)
 
             if opt.alpha != 0:
-                Z_opt = noise_amp*z_opt + z_prev
+                Z_opt = noise_amp*z_opt + prev_recon
                 #               -->         z_opt = 0 ({Z*,0,0,0,0,0})
                 loss_criterion = nn.MSELoss()
-                #rec_loss = (5**cur_scale) * opt.alpha * loss_criterion(curr_G(Z_opt.detach(), z_prev), real_img)
-                rec_loss = loss_criterion(curr_g(Z_opt.detach(), z_prev), real_img)
+                rec_loss = loss_criterion(curr_g(Z_opt.detach(), prev_recon), real_img)
                 # rec_loss.backward(retain_graph=True)
-                # rec_loss = rec_loss.detach()
+                rec_loss_arr.append(rec_loss.detach())
             else:
                 Z_opt = noise_amp*z_opt
                 rec_loss = 0
@@ -249,12 +254,15 @@ def _train_single_scale(trained_generators, z_opts, noise_amps, curr_g, real_img
             total_loss.backward(retain_graph=True)
             optimizer.step()
         scheduler.step()
-        # scheduler.step(total_loss)
-        rec_loss_arr.append(rec_loss.detach())
-        color_loss_arr.append(color_loss.detach() if opt.c_alpha else color_loss)
 
-        logging_dict = {f'scale_{cur_scale}': {'loss': distribution_loss_arr[-1], 'rec_loss': rec_loss_arr[-1], 'total_loss': total_loss.detach(), 'epoch': epoch},
-                        'running_total_loss': total_loss.detach(), 'running_rec_loss': rec_loss_arr[-1], 'running_loss': distribution_loss_arr[-1]}
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ LOGGING  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        logging_dict = {f'scale_{cur_scale}': {'loss': distribution_loss_arr[-1],
+                                               'rec_loss': rec_loss_arr[-1],
+                                               'total_loss': total_loss.detach(),
+                                               'epoch': epoch},
+                        'running_total_loss': total_loss.detach(),
+                        'running_rec_loss': rec_loss_arr[-1],
+                        'running_loss': distribution_loss_arr[-1]}
 
         if epoch % opt.epoch_print == 0:
             print_line = f"epoch {epoch}:\t{opt.loss_func}:%.2e \t Rec:%.2e \t Color:%.2e \t" \
@@ -268,20 +276,20 @@ def _train_single_scale(trained_generators, z_opts, noise_amps, curr_g, real_img
         if opt.epoch_show != -1 and epoch % opt.epoch_show == 0:
             # ==== SOME BUGS IN COMMENTS (INPUT TO CURR_G SHOULD BE NOISEAMP*NOISE+PREV)  ====
             # ====                  UNCOMMENT CAREFULLY                                   ====
-            # example_fake = curr_G(example_noise, prev)
+            # example_fake = curr_G(example_noise, prev_rand)
             # plotting_helpers.show_im(example_fake, title=f'e{epoch} epoch')
-            # details_fake = curr_G(example_noise, z_prev)
+            # details_fake = curr_G(example_noise, prev_recon)
             # plotting_helpers.show_im(details_fake, title=f'Details {epoch} epoch')
-            z_opt_fake = curr_g(Z_opt, z_prev)
+            z_opt_fake = curr_g(Z_opt, prev_recon)
             plotting_helpers.show_im(z_opt_fake, title=f'Zopt_e{epoch} epoch')
         if epoch % opt.epoch_save == 0:
-            # example_fake = curr_G(example_noise, prev)
+            # example_fake = curr_G(example_noise, prev_rand)
             # plotting_helpers.save_im(example_fake, out_dir, f'e{epoch}', convert=True)
-            ex_noise = example_noise*noise_amp + z_prev
-            details_fake = curr_g(ex_noise, z_prev)
+            ex_noise = example_noise*noise_amp + prev_recon
+            details_fake = curr_g(ex_noise, prev_recon)
             details_fake_wandb = wandb.Image(plotting_helpers.convert_im(details_fake), caption=f'Details_e{epoch}')
             # plotting_helpers.save_im(details_fake, out_dir, f'Details_e{epoch}', convert=True)
-            z_opt_fake = curr_g(Z_opt, z_prev)
+            z_opt_fake = curr_g(Z_opt, prev_recon)
             z_opt_fake_wandb = wandb.Image(plotting_helpers.convert_im(z_opt_fake), caption=f'Zopt_e{epoch}')
             logging_dict[f'scale_{cur_scale}']['Z_opt']= z_opt_fake_wandb
             logging_dict[f'scale_{cur_scale}']['Details_fake'] = details_fake_wandb
@@ -291,10 +299,10 @@ def _train_single_scale(trained_generators, z_opts, noise_amps, curr_g, real_img
         #           'scale': cur_scale})
         wandb.log(logging_dict)
 
-        # update prev
-        prev = _draw_concat(trained_generators, z_opts, real_imgs, noise_amps, 'rand', pad_func,
+        # update prev_rand
+        prev_rand = _draw_concat(trained_generators, z_opts, real_imgs, noise_amps, 'rand', pad_func,
                             scale_factor, opt)
-        prev = pad_func(prev)
+        prev_rand = pad_func(prev_rand)
 
     # ========================================== END OF ALL EPOCHS ================================================
     # TODO save network?
@@ -304,17 +312,17 @@ def _train_single_scale(trained_generators, z_opts, noise_amps, curr_g, real_img
     images_wandb_all = []
     # ======== GENERATE THIS SCALE'S RANDOM SAMPLES =========
     for i in range(opt.generate_fake_amount):
-        # Generate an image using the same "prev" image (i.e., only the last layer changes stuff)
+        # Generate an image using the same "prev_rand" image (i.e., only the last layer changes stuff)
         if cur_scale == 0:
             example_noise = image_processing.generate_noise([1, opt.nzx, opt.nzy]).detach()
             example_noise = example_noise.expand(1, opt.nc, opt.nzx, opt.nzy)
         else:
             example_noise = image_processing.generate_noise([opt.nc, opt.nzx, opt.nzy]).detach()
         example_noise = pad_func(example_noise)
-        z_in = noise_amp * example_noise + prev
-        example_fake = curr_g(z_in, prev)
+        z_in = noise_amp * example_noise + prev_rand
+        example_fake = curr_g(z_in, prev_rand)
 
-        # Generate an image using a random "prev" image (i.e., the entire image should be different)
+        # Generate an image using a random "prev_rand" image (i.e., the entire image should be different)
         example_prev = _draw_concat(trained_generators, z_opts, real_imgs, noise_amps, 'rand',
                                     pad_func, scale_factor, opt)
         example_prev = pad_func(example_prev)
@@ -322,7 +330,7 @@ def _train_single_scale(trained_generators, z_opts, noise_amps, curr_g, real_img
         example_fake_all = curr_g(z_in, example_prev)
 
         if opt.epoch_show != -1:
-            fim = plotting_helpers.show_im(example_fake, title=f'Final Image - Same prev {i}')
+            fim = plotting_helpers.show_im(example_fake, title=f'Final Image - Same prev_rand {i}')
             plotting_helpers.save_im(fim, out_dir, f'fake_samePrev{i}')
             faim = plotting_helpers.show_im(example_fake_all, title=f'Final Image{i}')
             plotting_helpers.save_im(faim, out_dir, f'fake_{i}')
@@ -333,8 +341,8 @@ def _train_single_scale(trained_generators, z_opts, noise_amps, curr_g, real_img
         images_wandb_all.append(wandb.Image(plotting_helpers.convert_im(example_fake_all), caption=f'fake_{i}'))
     wandb.log({f'example_fake_{cur_scale}': images_wandb, f'example_fake_all_{cur_scale}': images_wandb_all})
 
-    # details_fake = curr_G(example_noise, z_prev)
-    z_opt_fake = curr_g(Z_opt, z_prev)
+    # details_fake = curr_G(example_noise, prev_recon)
+    z_opt_fake = curr_g(Z_opt, prev_recon)
     if opt.epoch_show != -1:
         # fim = plotting_helpers.show_im(example_fake, title='Final Image')
         # dim = plotting_helpers.show_im(details_fake, title='Final Details Image')
